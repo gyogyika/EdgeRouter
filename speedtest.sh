5c0038d27be4689a22a22159687563f5f8012eaa
root <root@gyogyi.OpenWrt>
2021-03-03 10:21:31 +0100

    speedtest better upload test

--- speedtest.sh
+++ speedtest.sh
@@ -6,30 +6,47 @@ read -r IPERF3 < "/root/iperf3"
 read -r SPEEDTEST_URL < "/root/speedtest_url"
 DOWNLOADSPEED=""
 UPLOADSPEED=""
-COUNT=0
+COUNTDOWNLOAD=0
+COUNTUPLOAD=0
 
-while [ -z "$DOWNLOADSPEED" ] && [ $COUNT -lt 5 ]
+while [ -z "$DOWNLOADSPEED" ] && [ $COUNTDOWNLOAD -lt 5 ]
 do
- COUNT=$((COUNT+1))
- echo Download test: $COUNT
+ COUNTDOWNLOAD=$((COUNTDOWNLOAD+1))
+ echo Download test: $COUNTDOWNLOAD
  DOWNLOADSPEED=$(iperf3 -c $IPERF3 -f m -R | awk '/receiver/{print$7,$8}')
  echo Download speed: ="$DOWNLOADSPEED"=
  if [ -z "$DOWNLOADSPEED" ]
  then
   sleep 2
- else
-  sleep 2
-  echo Upload test: $COUNT
-  UPLOADSPEED=$(iperf3 -c $IPERF3 -f m | awk '/receiver/{print$7,$8}')
-  echo Upload speed: ="$UPLOADSPEED"=
  fi
 done
 
-if [ $COUNT -gt 4 ]
+sleep 2
+
+while [ -z "$UPLOADSPEED" ] && [ $COUNTUPLOAD -lt 5 ]
+do
+ COUNTUPLOAD=$((COUNTUPLOAD+1))
+ echo Upload test: $COUNTUPLOAD
+ UPLOADSPEED=$(iperf3 -c $IPERF3 -f m | awk '/receiver/{print$7,$8}')
+ echo Upload speed: ="$UPLOADSPEED"=
+ if [ -z "$UPLOADSPEED" ]
+ then
+  sleep 2
+ fi
+done
+
+if [ $COUNTDOWNLOAD -gt 4 ]
 then
-  echo No speedtest server "$IPERF3".
-  #$SENDMAIL "No speedtest server $IPERF3" "No speedtest server $IPERF3"
+  echo No download speedtest server "$IPERF3".
+  #$SENDMAIL "No download speedtest server $IPERF3" "No speedtest server $IPERF3"
 fi
+
+if [ $COUNTUPLOAD -gt 4 ]
+then
+  echo No upload speedtest server "$IPERF3".
+  #$SENDMAIL "No upload speedtest server $IPERF3" "No speedtest server $IPERF3"
+fi
+
 
 curl -G \
   --data-urlencode "name=$ROUTERNAME" \
