6a37f30c0cd0dcdeb289777845649fccb07f7e9a
root <root@gyogyi.OpenWrt>
2021-05-08 12:05:56 +0200

    test

--- t.sh
+++ t.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 
-VPNuserloggedin() {
-  RET=$(awk '/pomocvpnclient/ {print}' /var/run/openvpn.pomocvpn.status)
-  echo =$RET=
-  if [ -z "$RET" ]
-  then
-    #not loggen in
-    # 1 = false
-    return 1
-  else
-    #loggen in
-    # 0 = true
-    return 0
-  fi
-}
+read -r IPERF3_PORT < "/root/iperf3_port"
 
-if VPNuserloggedin;
-then
-  echo Logged in.
-else
-  echo Not logged in.
-fi
+IPERF3_PORT=$((IPERF3_PORT+1))
 
+echo $IPERF3_PORT > "/root/iperf3_port"
