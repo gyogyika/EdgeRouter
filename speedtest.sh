29fdc32f59da15b5274fdbdc1fa9bf2c1855ad21
root <root@gyogyi.OpenWrt>
2021-05-08 11:19:58 +0200

    Iperf3 port auto increment

--- speedtest.sh
+++ speedtest.sh
@@ -3,21 +3,33 @@
 SENDMAIL="/root/send-mail.sh"
 read -r ROUTERNAME < "/root/routername"
 read -r IPERF3 < "/root/iperf3"
+read -r IPERF3_PORT < "/root/iperf3_port"
 read -r SPEEDTEST_URL < "/root/speedtest_url"
 DOWNLOADSPEED=""
 UPLOADSPEED=""
 COUNTDOWNLOAD=0
 COUNTUPLOAD=0
 
+incport() {
+  IPERF3_PORT=$((IPERF3_PORT+1))
+  if [ $IPERF3_PORT -gt 9240 ] || [ $IPERF3_PORT -lt 9200 ]
+  then
+    IPERF3_PORT=9200
+  fi
+  echo $IPERF3_PORT > "/root/iperf3_port"
+}
+
+
 while [ -z "$DOWNLOADSPEED" ] && [ $COUNTDOWNLOAD -lt 5 ]
 do
  COUNTDOWNLOAD=$((COUNTDOWNLOAD+1))
- echo Download test: $COUNTDOWNLOAD
- DOWNLOADSPEED=$(iperf3 -c $IPERF3 -f m -R | awk '/receiver/{print$7,$8}')
+ echo Download test: $COUNTDOWNLOAD, Server: $IPERF3 $IPERF3_PORT
+ DOWNLOADSPEED=$(iperf3 -c $IPERF3 $IPERF3_PORT -f m -R | awk '/receiver/{print$7,$8}')
  echo Download speed: ="$DOWNLOADSPEED"=
  if [ -z "$DOWNLOADSPEED" ]
  then
   sleep 2
+  incport
  fi
 done
 
@@ -26,12 +38,13 @@ sleep 2
 while [ -z "$UPLOADSPEED" ] && [ $COUNTUPLOAD -lt 5 ]
 do
  COUNTUPLOAD=$((COUNTUPLOAD+1))
- echo Upload test: $COUNTUPLOAD
- UPLOADSPEED=$(iperf3 -c $IPERF3 -f m | awk '/receiver/{print$7,$8}')
+ echo Upload test: $COUNTUPLOAD, Server: $IPERF3 $IPERF3_PORT
+ UPLOADSPEED=$(iperf3 -c $IPERF3 $IPERF3_PORT -f m | awk '/receiver/{print$7,$8}')
  echo Upload speed: ="$UPLOADSPEED"=
  if [ -z "$UPLOADSPEED" ]
  then
   sleep 2
+  incport
  fi
 done
 
@@ -46,7 +59,6 @@ then
   echo No upload speedtest server "$IPERF3".
   #$SENDMAIL "No upload speedtest server $IPERF3" "No speedtest server $IPERF3"
 fi
-
 
 curl -G \
   --data-urlencode "name=$ROUTERNAME" \
