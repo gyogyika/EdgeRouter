dd92117179b05091e514f69ce0c6150d35d37a6b
root <root@gyogyi.OpenWrt>
2021-08-07 18:40:40 +0200

    added dhcp.sh

---
+++ dhcp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#send-mail.sh "Send-dhcp.sh - test" "test"
+
+FILE1="/root/dhcp-current.log"
+FILE2="/root/dhcp-previous.log"
+SENDMAIL="/root/send-mail.sh"
+
+awk '/DHCPDISCOVER/ {print}' /tmp/system.log | sort -u > $FILE1
+
+if cmp -s "$FILE1" "$FILE2"
+then
+    printf 'The file "%s" is the same as "%s"\n' "$FILE1" "$FILE2"
+else
+    cp $FILE1 $FILE2
+    echo "$FILE1"
+    echo "changed to"
+    echo "$FILE2"
+    $SENDMAIL "dhcp request" "$(cat $FILE1)"
+fi
