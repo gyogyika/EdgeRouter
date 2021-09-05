e7fe3fb8ac7bb07d23ed5f27aae968e900ddccaa
root <root@gyogyi.OpenWrt>
2021-09-05 19:11:34 +0200

    speedtestlocal updated

--- speedtestlocal.sh
+++ speedtestlocal.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-iperf3 -c localhost -f m -R | awk '/receiver/{print$7,$8}'
+
+#running as server
+iperf3 -s -f M
+
+#client usage
+#-R reverse mode, where the server sends and the client receives
+#iperf3 -c 192.168.1.1 -f M -R
