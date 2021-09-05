462d08870c22c0fae82bb2c2b03f5c403fb91f3b
root <root@gyogyi.OpenWrt>
2021-09-05 17:38:30 +0200

    vpnstatus improved

--- vpnstatus.sh
+++ vpnstatus.sh
@@ -1,33 +1,30 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-echo "$1"
-
-C=""
-
-ROWS=$(ls /var/run/openvpn.*.status | wc -l)
-echo "Number of rows: " $ROWS
-
-I=0;
-
-for F in /var/run/openvpn.*.status;
-  do
-    #echo $F;
-    C="$C echo $F && cat $F";
-    I=$((I+1))
-    if [ $I -lt $ROWS ]
-    then
-      C="$C && echo && ";
-    fi
-done;
-
-#echo $C
-watch -n1 $C
-fi
-
 if [ "$1" != "" ]
 then
   VPNSTATUS=/var/run/openvpn.$1.status
   watch -n1 "echo $VPNSTATUS && cat $VPNSTATUS"
+else
+  echo "$1"
+
+  COMMANDS=""
+
+  ROWS=$(find /var/run/openvpn.*.status | wc -l)
+  echo "Number of rows: $ROWS"
+
+  I=0;
+
+  for F in /var/run/openvpn.*.status;
+  do
+    #echo $F;
+    COMMANDS="$COMMANDS echo $F && cat $F";
+    I=$((I+1))
+    if [ $I -lt "$ROWS" ]
+    then
+      COMMANDS="$COMMANDS && echo && ";
+    fi
+  done;
+
+  #echo $COMMANDS
+  watch -n1 "$COMMANDS"
 fi
