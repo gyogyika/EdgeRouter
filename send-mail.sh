3eaa0c25e87b4fa86c363519bc3c44fd23caa0c5
root <root@gyogyi.OpenWrt>
2022-03-09 08:38:01 +0100

    send via http

--- send-mail.sh
+++ send-mail.sh
@@ -16,6 +16,13 @@ else
  MESSAGE=$2
 fi
 
-mailsend -f "$SENDMAIL_FROM" -t "$SENDMAIL_TO" -sub "$SUBJECT" -M "$MESSAGE" -smtp localhost
+curl -G \
+  --data-urlencode "from=$SENDMAIL_FROM" \
+  --data-urlencode "subject=$SUBJECT" \
+  --data-urlencode "message=$MESSAGE" \
+  "$SENDMAIL_URL"
+
+#mailsend -f "$SENDMAIL_FROM" -t "$SENDMAIL_TO" -sub "$SUBJECT" -M "$MESSAGE" -smtp localhost
+
 NOW=$(date +"%d.%m.%Y %A, %H:%M")
 echo "$NOW - $1" >> /tmp/send-mail.log
