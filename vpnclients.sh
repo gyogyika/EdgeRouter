b4db107de1fded798cfdff77a770869f9af7bfab
root <root@gyogyi.OpenWrt>
2022-05-10 13:10:01 +0200

    improved vpnclient

--- vpnclients.sh
+++ vpnclients.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-VPNRESULT="/tmp/VPNCLIENTS"
+VPNCLIENTS="/tmp/VPNCLIENTS"
 
-cat /dev/null > "$VPNRESULT"
+cat /dev/null > "$VPNCLIENTS"
 
 for SERVERNAME in /var/run/openvpn.*.status;
   do
     #echo "$SERVERNAME"
     #awk -F: - field separator for : sign
-    awk -F: '/^Common/{flag=1;next} /^ROUTING TABLE/{flag=0} flag {print $1}' "$SERVERNAME" >> "$VPNRESULT"
+    #awk -F: '/^Common/{flag=1;next} /^ROUTING TABLE/{flag=0} flag {print $1}' "$SERVERNAME" >> "$VPNCLIENTS"
+    awk -F: '/^Virtual Address/{flag=1;next} /^GLOBAL STATS/{flag=0} flag {print $1}' "$SERVERNAME" >> "$VPNCLIENTS"
   done;
 
-cat $VPNRESULT
+cat $VPNCLIENTS
