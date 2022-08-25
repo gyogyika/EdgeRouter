8251c0ac62cca9ad72acdce6d1ff50656835e88a
root <root@gyogyi.OpenWrt>
2022-08-25 17:32:20 +0200

    pingtofile added

--- www/stk.php
+++ www/stk.php
@@ -7,11 +7,18 @@ function ping($host) {
   return $rval === 0;
 }
 
+function echoln($string) {
+  echo $string . PHP_EOL;
+}
+
+
   $name = array();
   $interface = array();
   $ips = array();
 
-  $lines = file('../pingto2');
+  $pingtofile = trim(file_get_contents('./pingtofile'));
+
+  $lines = file($pingtofile);
 
   foreach ($lines as $line_num => $line) {
     if ((strpos($line, '#') === 0) or (trim($line) == '')) {
@@ -24,19 +31,18 @@ function ping($host) {
         $ips[] = trim($pieces[2]);
       }
   }
-  
-  echo '<body>';
-  echo '<table>';
-  echo '<tr><th>Num</th><th>Device</th><th>Interface</th><th>IP address</th><th>Ping status</th>' . PHP_EOL;
+
+  echoln ('<body>');
+  echoln ('<table>');
+  echoln ('<tr><th>Num</th><th>Device</th><th>Interface</th><th>IP address</th><th>Ping status</th>');
   foreach ($ips as $num => $ip) {
     $ping = (ping($ip)) ? 'ping OK' : 'NO ping';
     $pingclass = ($ping == 'ping OK') ? 'online' : 'offline';
-    echo '<tr><td>' . ($num + 1) . '</td><td>' . $name[$num] . '</td><td>' . $interface[$num] . '</td><td>' . $ip . '</td><td class="' . $pingclass . '">' . $ping . '</td></tr>' . PHP_EOL;
+    echoln ('<tr><td>' . ($num + 1) . '</td><td>' . $name[$num] . '</td><td>' . $interface[$num] . '</td><td>' . $ip . '</td><td class="' . $pingclass . '">' . $ping . '</td></tr>');
   }
 
-  echo '</table>' . PHP_EOL;
-  echo '</body>' . PHP_EOL;
-  echo '</html>' . PHP_EOL;
+  echoln ('</table>');
+  echoln ('</body>');
+  echoln ('</html>');
 
 ?>
-
