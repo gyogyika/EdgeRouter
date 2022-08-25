05252d00e420683a05be1d75a75ad902bb588ed6
root <root@gyogyi.OpenWrt>
2022-08-25 16:41:38 +0200

    hover added

--- www/stk.php
+++ www/stk.php
@@ -27,11 +27,11 @@ function ping($host) {
   
   echo '<body>';
   echo '<table>';
-  echo '<tr><th>Device</th><th>Interface</th><th>IP address</th><th>Ping status</th>' . PHP_EOL;
+  echo '<tr><th>Num</th><th>Device</th><th>Interface</th><th>IP address</th><th>Ping status</th>' . PHP_EOL;
   foreach ($ips as $num => $ip) {
     $ping = (ping($ip)) ? 'ping OK' : 'NO ping';
     $pingclass = ($ping == 'ping OK') ? 'online' : 'offline';
-    echo '<tr><td>' . $name[$num] . '</td><td>' . $interface[$num] . '</td><td>' . $ip . '</td><td class="' . $pingclass . '">' . $ping . '</td></tr>' . PHP_EOL;
+    echo '<tr><td>' . ($num + 1) . '</td><td>' . $name[$num] . '</td><td>' . $interface[$num] . '</td><td>' . $ip . '</td><td class="' . $pingclass . '">' . $ping . '</td></tr>' . PHP_EOL;
   }
 
   echo '</table>' . PHP_EOL;
