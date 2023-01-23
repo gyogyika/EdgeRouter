4a2cf9d460da6c08bc79f6f4ec031798d3e1c50a
root <root@TSOFT.EdgeRouterX.OpenWrt>
2023-01-23 12:52:13 +0100

    freemyip check

--- GetIP.sh
+++ GetIP.sh
@@ -5,14 +5,20 @@ source /root/settings.ini
 echo
 URL="$GETIP_URL"?name="$ROUTERNAME"
 echo "GetIP:" "$URL"
-curl --max-time 5 "$URL"
+curl --max-time "$CURL_TIMEOUT" "$URL"
 echo
 echo
 
-echo "freemyip:" "$FREEMYIP"
-curl --max-time 5 "$FREEMYIP"
-echo
+if [ -n "$FREEMYIP" ]
+then
+  echo "freemyip:" "$FREEMYIP"
+  curl --max-time "$CURL_TIMEOUT" "$FREEMYIP"
+  echo
+fi
 
-echo "freemyip2:" "$FREEMYIP2"
-curl --max-time 5 "$FREEMYIP2"
-echo
+if [ -n "$FREEMYIP2" ]
+then
+  echo "freemyip2:" "$FREEMYIP2"
+  curl --max-time "$CURL_TIMEOUT" "$FREEMYIP2"
+  echo
+fi
