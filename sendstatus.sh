e25cdf16e98013333948a84f41844f065c5c8c80
root <root@TSOFT.EdgeRouterX.OpenWrt>
2023-07-05 21:15:17 +0200

    UPS status added

--- sendstatus.sh
+++ sendstatus.sh
@@ -15,6 +15,15 @@ get_wan_status "WAN3"
 WAN3_STATUS=$WAN_STATUS
 echo WAN3: $WAN3_STATUS
 
+MACHINE=$(cat /proc/cpuinfo | awk -F': ' '/machine/ {print $2}')
+echo MACHINE: $MACHINE
+
+SYSTEM_TYPE=$(cat /proc/cpuinfo | awk -F': ' '/system type/ {print $2}')
+echo SYSTEM_TYPE: $SYSTEM_TYPE
+
+CPU_CORES=$(cat /proc/cpuinfo | awk -F: '/processor/ {print $0}' | wc -l)
+echo CPU_CORES: $CPU_CORES
+
 NOPINGS="invalid"
 read -r NOPINGS < "/tmp/NOPINGS"
 echo NOPINGS: $NOPINGS
@@ -68,12 +77,51 @@ echo "OpenWrt: $OpenWrt"
 
 TIME=$(date +%s)
 
+if [ "$UPS_ENABLED" = "1" ]
+then
+  echo "UPS_ENABLED: $UPS_ENABLED"
+  UPS=$(upsc ups)
+
+  UPS_model=$(echo "$UPS" | awk '/ups.model:/{$1="";print $0}')
+  echo "UPS_model: $UPS_model"
+
+  if [ -z "$UPS_battery_date" ]
+  then
+    UPS_battery_date=$(echo "$UPS" | awk '/battery.mfr.date:/{$1="";print $0}')
+  fi
+  echo "UPS_battery_date: $UPS_battery_date"
+
+  UPS_battery_charge=$(echo "$UPS" | awk '/battery.charge:/{$1="";print $0}')
+  echo "UPS_battery_charge: $UPS_battery_charge"
+
+  UPS_battery_time=$(echo "$UPS" | awk '/battery.runtime:/{$1="";print $0}')
+  echo "UPS_battery_time: $UPS_battery_time"
+
+  UPS_battery_voltage=$(echo "$UPS" | awk '/battery.voltage:/{$1="";print $0}')
+  echo "UPS_battery_voltage: $UPS_battery_voltage"
+
+  UPS_load=$(echo "$UPS" | awk '/ups.load:/{$1="";print $0}')
+  echo "UPS_load: $UPS_load"
+
+  UPS_voltage=$(echo "$UPS" | awk '/input.voltage:/{$1="";print $0}')
+  echo "UPS_voltage: $UPS_voltage"
+
+  UPS_status=$(echo "$UPS" | awk '/ups.status:/{$1="";print $0}')
+  echo "UPS_status: $UPS_status"
+
+  UPS_date=$(echo "$UPS" | awk '/ups.mfr.date:/{$1="";print $0}')
+  echo "UPS_date: $UPS_date"
+fi
+
 curl --get \
   --data-urlencode "set=router" \
   --data-urlencode "name=$ROUTERNAME" \
   --data-urlencode "WAN1=$WAN1_STATUS" \
   --data-urlencode "WAN2=$WAN2_STATUS" \
   --data-urlencode "WAN3=$WAN3_STATUS" \
+  --data-urlencode "MACHINE=$MACHINE" \
+  --data-urlencode "SYSTEM_TYPE=$SYSTEM_TYPE" \
+  --data-urlencode "CPU_CORES=$CPU_CORES" \
   --data-urlencode "PINGS=$PINGS" \
   --data-urlencode "NOPINGS=$NOPINGS" \
   --data-urlencode "NOPINGS_CRIT=$NOPINGS_CRIT" \
@@ -88,4 +136,13 @@ curl --get \
   --data-urlencode "CPU_load=$CPU_load" \
   --data-urlencode "Storage_load=$Storage_load" \
   --data-urlencode "OpenWrt=$OpenWrt" \
+  --data-urlencode "UPS_model=$UPS_model" \
+  --data-urlencode "UPS_battery_date=$UPS_battery_date" \
+  --data-urlencode "UPS_battery_charge=$UPS_battery_charge" \
+  --data-urlencode "UPS_battery_time=$UPS_battery_time" \
+  --data-urlencode "UPS_battery_voltage=$UPS_battery_voltage" \
+  --data-urlencode "UPS_load=$UPS_load" \
+  --data-urlencode "UPS_voltage=$UPS_voltage" \
+  --data-urlencode "UPS_status=$UPS_status" \
+  --data-urlencode "UPS_date=$UPS_date" \
 "$STATUS_URL"
