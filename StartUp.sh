0cb44a59cb02d98cb2d4b5911cad04fb4e31aa47
root <root@TSOFT.EdgeRouterX.OpenWrt>
2025-01-22 06:55:28 +0100

    freemyip update after IP change and at startup

--- StartUp.sh
+++ StartUp.sh
@@ -3,7 +3,8 @@
 bash "/root/copytotmp.sh"
 sleep 10
 source /tmp/root/settings.ini
-bash "/tmp/root/settime.sh"
+bash "/root/settime.sh"
 bash $SENDMAIL "Router started" "$(date)"
 bash "/tmp/root/cron1minute.sh"
 bash "/tmp/root/cron10minute.sh"
+bash "/tmp/root/freemyip.sh"
